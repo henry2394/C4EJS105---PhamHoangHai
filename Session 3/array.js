a475f99adfac0f8642eaa4531805fc63d07a9f3e
henry2394 <hoanghai.ufba@gmail.com>
2020-12-24 08:50:42 +0700

    array.js

---
+++ Session 3/array.js
@@ -0,0 +1,70 @@
+// Init
+let movie = ['the walking dead', 'friends', 'how i met your mother'];
+// console.log(movie)
+// Create
+// movie.push('them 1 phim')
+// console.log(movie) 
+// Read
+// console.log(movie[1])
+
+// Update 1
+// var nhap_ten = prompt("nhap ten phim")
+// movie[1] = nhap_ten
+// console.log(movie)
+
+//update 2
+// var nhap_ten = prompt("nhap ten phim")
+// var nhap_vitri = prompt("nhap vi tri")
+
+// movie[nhap_vitri] = nhap_ten
+// console.log(movie)
+
+// delete 1
+// let vi_tri_xoa = prompt("nhap index muon xoa")
+// movie.splice(vi_tri_xoa,1)
+
+// delete 2
+// let vi_tri_xoa = prompt("nhap index muon xoa")
+// let so_luong_xoa = prompt("nhap so luong xoa")
+// movie.splice(vi_tri_xoa,so_luong_xoa)
+
+//Read All
+
+// Read or log all of items in movies array into console
+// for (i =0 ; i < movie.length ; i++) {
+//     console.log(movie[i])
+// }
+
+// Read or log only first half items in movies array into console
+// for (i =0 ; i < (movie.length/2) ; i++) {
+//     console.log(movie[i])
+// }
+
+//Read or log all of items in movies array into console with the item position,
+
+// for (i =0 ; i <= movie.length ; i++) {
+//     console.log(i + ". "+movie[i])
+// }
+
+// Update All
+// Change all items of movies array into lowercase/uppercase
+// for (i =0 ; i < movie.length ; i++) {
+//     movie[i] = movie[i].toUpperCase();
+// }
+// console.log(movie)
+
+// Purpose of While 
+// check name
+// var nhap_ten = prompt("nhap ten")
+// if (nhap_ten.length > 15) {
+//     alert("your name is too long")
+// }
+// else alert("good")
+
+
+//a quiz with 4 choices,
+var nhap_dap_an = prompt("nhap dap an")
+if (nhap_dap_an  in [1,2,3,4]) {
+    alert("oke")
+}
+else alert("???")
